621853479f5a2146179163f8cabb22e6f1f353c0
Arun <arunachalam944@gmail.com>
2021-10-15 06:14:20 -0700

    initial commit

---
+++ src/main/java/com/kubernetesConfigMapExample/KubernetesConfigMapExampleApplication.java
@@ -0,0 +1,13 @@
+package com.kubernetesConfigMapExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesConfigMapExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KubernetesConfigMapExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kubernetesConfigMapExample/KubernetesConfigMapExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubernetesConfigMapExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernetesConfigMapExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
